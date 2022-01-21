73cd8dd062be2d19f136f21e7e5cabec898f85b0
Even Pan <evenpan@fareastone.com.tw>
2022-01-21 16:35:37 +0800

    [revise] solve year floating number, remove lambda, add comment why: 1. year should be int 2. lambda seems messy, try better way 3. add comment how: 1. remove nan value in year and astype it as int 2. use str.contains to do the same things 3. add comment to explain each block of code

--- imdb_selector.py
+++ imdb_selector.py
@@ -1,47 +1,68 @@
 """
 # My first app
-Here's our first attempt at using data to create a table:
+DESC: app for IMDB movie recommendation
+user inputs: min_year, max_year, genre
+output: top ten IMDB_score movie (with title/year/genre/score)
 """
-
 import streamlit as st
 import pandas as pd
+
+
+NA_FILL_VALUE = -1
 
 @st.cache
 def get_df():
     df = pd.read_csv('movie_metadata.csv')
+    df= df.dropna(subset=['title_year'])
+    df['title_year'] = df.title_year.astype(int)
     return df
+
 
 def get_unique_genres(df):
     all_unique_genres = df.genres.unique()
     unique_genres = set()
     for multi_genres in all_unique_genres:
         unique_genres.update(multi_genres.split('|'))
-    return unique_genres
+    return sorted(unique_genres)
 
+
+#use cache dataframe
 movie_table = get_df()
-
-
+#get min and max year from table 
 table_min_year = int(movie_table.title_year.min())
 table_max_year = int(movie_table.title_year.max())
 
-st.write(f'min year: {table_min_year}, max year: {table_max_year}')
+#get data from streamlit
+user_max_year = st.slider('movie not more than ...', min_value=table_min_year, max_value=table_max_year, value=2000)
+user_min_year = st.slider('movie not less than ...', min_value=table_min_year, max_value=table_max_year, value=1990)
 
-max_year = st.slider('movie not more than ...', min_value=table_min_year, max_value=table_max_year, value=2000)
-min_year = st.slider('movie not less than ...', min_value=table_min_year, max_value=table_max_year, value=1990)
+# switch two boundary if user misunderstanding
+if user_max_year < user_min_year:
+   user_max_year, user_min_year = user_min_year, user_max_year
 
+# get unique genres form table
 unique_genres = get_unique_genres(movie_table)
 
+# let user select genre
 selected_genres = st.selectbox('select the genre you like', unique_genres)
 
-st.write(f'max year: {max_year}, min year: {min_year}, selected genres: {selected_genres}')
+# print out current result
+st.write(f'your selection: max year: {user_max_year}, min year: {user_min_year}, selected genres: {selected_genres}')
 
-year_lower = movie_table.title_year >= min_year
-year_upper = movie_table.title_year < max_year
 
-year_constrain = movie_table[year_lower & year_upper]
+# set two masks for filter out data user want
+year_lower_mask = movie_table.title_year >= user_min_year
+year_upper_mask = movie_table.title_year < user_max_year
 
-year_constrain['genres_sellected'] = year_constrain.genres.apply(lambda x: selected_genres in x)
+# select data stand within the period user interested
+year_constrain = movie_table[year_lower_mask & year_upper_mask]
+
+# make sure movie contain genre user selected
+year_constrain['genres_sellected'] = year_constrain.genres.str.contains(selected_genres)
 year_constrain_genre_selected = year_constrain[year_constrain.genres_sellected]
 
+# sorted the result by its imdb score
 scored_result = year_constrain_genre_selected.sort_values(by=['imdb_score'], ascending=False)
-st.write(scored_result[['movie_title', 'imdb_score', 'genres', 'title_year']])
+
+# print only top ten result by using head(10)
+st.write(scored_result[['movie_title', 'imdb_score', 'genres', 'title_year']].head(10))
