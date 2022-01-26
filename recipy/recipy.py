abe65b7407c507def4febef2aa7c4843f5131850
Even Pan <evenpan@fareastone.com.tw>
2022-01-26 23:50:43 +0800

    add recipy user input and responsive answer

--- recipy/recipy.py
+++ recipy/recipy.py
@@ -3,6 +3,7 @@
 DESC: app for Recipy recommendation
 
 """
+from lib2to3.pgen2.pgen import DFAState
 import shutil
 import streamlit as st
 import pandas as pd
@@ -13,11 +14,24 @@ with gzip.open('recipy/recipeitems-latest.json.gz', 'rb') as f_in:
         shutil.copyfileobj(f_in, f_out)
 
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def get_df():
     df = pd.read_json('recipy/recipeitems-latest.json', lines=True)
+    df['description'] = df.description.str.lower()
     return df
 
 recipes = get_df()
-st.write(recipes.head(10))
-st.write(len(recipes))
+
+st.write('recipes quantity: ', len(recipes))
+
+
+spice_list = ['salt', 'pepper', 'oregano', 'sage', 'parsley', 'rosemary', 'tarragon', 'thyme', 'paprika', 'cumin']
+options = st.multiselect("enter which spice you are interested(multi-select)", spice_list)
+
+
+spice_df = pd.DataFrame(dict((spice, recipes.ingredients.str.contains(spice)) for spice in spice_list))
+
+selection = spice_df.query(' & '.join(options))
+st.write(len(selection))
+
+st.write(recipes.name[selection.index])
