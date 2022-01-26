4d1faf2fbd81af0413a099fcdeb774cbc1a1ad69
Even Pan <evenpan@fareastone.com.tw>
2022-01-26 22:55:25 +0800

    add read file and inline cache

--- recipy/recipy.py
+++ recipy/recipy.py
@@ -11,3 +11,7 @@ import subprocess
 
 subprocess.run(["curl", "-L", "-o", "recipeitems-latest.json.gz", "'https://docs.google.com/uc?export=download&id=1hGSFmpptgZyX4jOal8B9LFDiLZACPSqO'"])
 subprocess.run(["gunzip", "recipeitems-latest.json.gz"])
+
+
+recipes = st.cache(pd.read_json('recipeitems-latest.json', lines=True))
+st.write(recipes.head())
