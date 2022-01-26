060ff7259fe71174329dda03655714c6b635b877
Even Pan <evenpan@fareastone.com.tw>
2022-01-26 23:08:58 +0800

    give up google download, try load file locally

--- recipy/recipy.py
+++ recipy/recipy.py
@@ -3,15 +3,21 @@
 DESC: app for Recipy recommendation
 
 """
+import shutil
 import streamlit as st
 import pandas as pd
-import subprocess
+import gzip
+
+with gzip.open('recipy/recipeitems-latest.json.gz', 'rb') as f_in:
+    with open('recipy/recipeitems-latest.json', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
 
 
+@st.cache
+def get_df():
+    df = pd.read_json('recipeitems-latest.json', lines=True)
+    return df
 
-subprocess.run(["curl", "-L", "-o", "recipeitems-latest.json.gz", "https://docs.google.com/uc?export=download&id=1hGSFmpptgZyX4jOal8B9LFDiLZACPSqO"])
-subprocess.run(["gunzip", "recipeitems-latest.json.gz"])
-
-
-recipes = st.cache(pd.read_json('recipeitems-latest.json', lines=True))
-st.write(recipes.head())
+recipes = get_df()
+st.write(recipes.head(10))
+st.write(len(recipes))
